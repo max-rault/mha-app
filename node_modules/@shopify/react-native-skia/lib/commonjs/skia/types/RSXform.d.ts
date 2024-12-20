59d47af38c144d4fb62a6d2f9c2886b78b8f5998
max-rault <maxime.rault65@gmail.com>
2024-12-20 16:45:42 +0100

    some update

--- node_modules/@shopify/react-native-skia/lib/commonjs/skia/types/RSXform.d.ts
+++
@@ -1,2 +0,0 @@
-import type { SkJSIInstance } from "./JsiInstance";
-export type SkRSXform = SkJSIInstance<"RSXform">;
--- node_modules/react-native-paper-dates/src/contexts/DisplayModeContext.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-
-export const DisplayModeContext = React.createContext<{
-  mode: 'AM' | 'PM' | undefined
-  setMode: React.Dispatch<React.SetStateAction<'AM' | 'PM' | undefined>>
-}>({ mode: 'AM', setMode: () => {} })
